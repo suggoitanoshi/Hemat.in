89299b4c831ab1bd2b2ff556132c0b08e9583031
Govindabhakta <igovinda100@gmail.com>
2020-08-10 23:35:31 +0700

    Tried to make chart take database values.
    Emphasis on tried

--- lib/main.dart
+++ lib/main.dart
@@ -44,69 +44,6 @@ class _MyHomePageState extends State<MyHomePage> {
   dynamic database;
   int state;
 
-  final List<SubscriberSeries> data = [
-    SubscriberSeries(
-      month: "Jan",
-      subscribers: 5000000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-    SubscriberSeries(
-      month: "Feb",
-      subscribers: 11000000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-    SubscriberSeries(
-      month: "Mar",
-      subscribers: 12000000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-    SubscriberSeries(
-      month: "Apr",
-      subscribers: 10000000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-    SubscriberSeries(
-      month: "May",
-      subscribers: 8500000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-    SubscriberSeries(
-      month: "Jun",
-      subscribers: 7700000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-    SubscriberSeries(
-      month: "Jul",
-      subscribers: 7600000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-    SubscriberSeries(
-      month: "Aug",
-      subscribers: 5500000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-    SubscriberSeries(
-      month: "Sep",
-      subscribers: 2500000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-    SubscriberSeries(
-      month: "Oct",
-      subscribers: 700000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-    SubscriberSeries(
-      month: "Nov",
-      subscribers: 4300000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-    SubscriberSeries(
-      month: "Dec",
-      subscribers: 1300000,
-      barColor: charts.ColorUtil.fromDartColor(Colors.blue),
-    ),
-  ];
-
   @override
   void initState() {
     super.initState();
@@ -126,7 +63,40 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text("Testing"),
+            // Card(
+            //     child: Padding(
+            //         padding: EdgeInsets.all(10),
+            //         child: Column(
+            //           children: <Widget>[
+            //             FutureBuilder(
+            //                 future: database.getAllExpenses(),
+            //                 builder: (BuildContext context,
+            //                     AsyncSnapshot<dynamic> snapshot) {
+            //                   if (!snapshot.hasData ||
+            //                       snapshot.data.length == 0) {
+            //                     return Text("No Data Has Been Set");
+            //                   } else {
+            //                     final List<SubscriberSeries> data = [
+            //                       for (final row in snapshot.data)
+            //                         {
+            //                           SubscriberSeries(
+            //                             month: row.date.month,
+            //                             subscribers: row.date.amount,
+            //                             barColor:
+            //                                 charts.ColorUtil.fromDartColor(
+            //                                     Colors.blue),
+            //                           )
+            //                         }
+            //                     ];
+
+            //                     return Scaffold(
+            //                       body: Center(
+            //                           child: SubscriberChart, data: data),
+            //                     );
+            //                   }
+            //                 })
+            //           ],
+            //         ))),
             Card(
               child: Padding(
                 padding: EdgeInsets.all(10),
