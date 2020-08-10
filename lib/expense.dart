a50d227bad1bfe046b5681b45d48ebd9fd5e0d2b
Govindabhakta <igovinda100@gmail.com>
2020-08-10 21:07:33 +0700

    ADDED UI slot for chart, comments for expense table

--- lib/expense.dart
+++ lib/expense.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
 
-
 class Pengeluaran extends StatelessWidget {
-  Widget build(context){
+  Widget build(context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Pengeluaran'),
+      appBar: AppBar(
+        title: Text('Pengeluaran'),
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,children: <Widget>[
-          Text('Bulan Ini',style: Theme.of(context).textTheme.headline3),
-          Card(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children:<Widget>[
-                Text('Nama',style: Theme.of(context).textTheme.headline6), 
-                Text('Sifat',style: Theme.of(context).textTheme.headline6), 
-                Text('Harga',style: Theme.of(context).textTheme.headline6)
-              ]
-            )
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Bulan Ini', style: Theme.of(context).textTheme.headline3),
+            Card(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                //DISINI BUAT YANG PIE CHART
+              ],
+            )),
+            Card(
+                //FOR FUNCTION, ITERATE ON THIS getThisMonthExpenses
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                  Text('Nama', style: Theme.of(context).textTheme.headline6),
+                  Text('Sifat', style: Theme.of(context).textTheme.headline6),
+                  Text('Harga', style: Theme.of(context).textTheme.headline6)
+                ])),
           ],
         ),
       ),
@@ -28,5 +36,4 @@ class Pengeluaran extends StatelessWidget {
   }
 }
 
-class SingleChildLayoutView {
-}
+class SingleChildLayoutView {}
