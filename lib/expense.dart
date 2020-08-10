ef966efb5a88893385ef03cc65c7631770ed25cf
jasonstanleyyoman <jasonstanleyyoman@gmail.com>
2020-08-10 21:59:52 +0700

    tabel pengeluaran

--- lib/expense.dart
+++ lib/expense.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
+import 'package:provider/provider.dart';
 
-class Pengeluaran extends StatelessWidget {
+import 'database/database.dart';
+
+class Pengeluaran extends StatefulWidget {
+  @override
+  _PengeluaranState createState() => _PengeluaranState();
+}
+
+class _PengeluaranState extends State<Pengeluaran> {
+  dynamic database;
+  @override
+  void initState() {
+    database = Provider.of<AppDatabase>(context, listen: false);
+  }
+
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
@@ -22,13 +36,42 @@ class Pengeluaran extends StatelessWidget {
             )),
             Card(
                 //FOR FUNCTION, ITERATE ON THIS getThisMonthExpenses
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                  Text('Nama', style: Theme.of(context).textTheme.headline6),
-                  Text('Sifat', style: Theme.of(context).textTheme.headline6),
-                  Text('Harga', style: Theme.of(context).textTheme.headline6)
-                ])),
+                child: FutureBuilder(
+              future: database.getAllExpenses(),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                final allExpense = snapshot.data;
+                if (allExpense.length == 0) {
+                  return Text("No Expense");
+                } else {
+                  List<DataRow> dataRowList = List<DataRow>();
+                  for (int i = 0; i < allExpense.length; i++) {
+                    final expense = allExpense[i];
+                    DataRow dataRow = DataRow(cells: <DataCell>[
+                      DataCell(Text(expense.title)),
+                      DataCell(Text(expense.amount.toString())),
+                      DataCell(Text(expense.date.toString())),
+                      DataCell(Text(
+                        expense.category,
+                        style: TextStyle(
+                            fontWeight: expense.category == "Ingin"
+                                ? FontWeight.bold
+                                : FontWeight.normal),
+                      )),
+                    ]);
+                    dataRowList.add(dataRow);
+                  }
+                  return DataTable(
+                    columns: <DataColumn>[
+                      DataColumn(label: Text("Title")),
+                      DataColumn(label: Text("Amount")),
+                      DataColumn(label: Text("Date")),
+                      DataColumn(label: Text("Jenis")),
+                    ],
+                    rows: dataRowList,
+                  );
+                }
+              },
+            )),
           ],
         ),
       ),
