103e7a0fe77627b72a77c833b46148e643f5025b
jasonstanleyyoman <jasonstanleyyoman@gmail.com>
2020-08-11 11:13:34 +0700

    Merapikan

--- lib/expense.dart
+++ lib/expense.dart
@@ -37,8 +37,8 @@ class _PengeluaranState extends State<Pengeluaran> {
                     "Jumlah : ",
                     style: TextStyle(fontWeight: FontWeight.w800, fontSize: 30),
                   ),
-                  FutureBuilder(
-                    future: database.getAllExpenses(),
+                  StreamBuilder(
+                    stream: database.watchAllExpenses(),
                     builder: (BuildContext context,
                         AsyncSnapshot<dynamic> snapshot) {
                       final allExpense = snapshot.data;
@@ -58,11 +58,24 @@ class _PengeluaranState extends State<Pengeluaran> {
                         Map<String, double> dataMap = new Map();
                         dataMap.putIfAbsent("Butuh", () => butuhCount);
                         dataMap.putIfAbsent("Ingin", () => inginCount);
-                        return SizedBox(
-                          height: 300,
-                          width: 300,
-                          child: PieChart(
-                              dataMap: dataMap, chartType: ChartType.disc),
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 32),
+                          child: Column(
+                            children: <Widget>[
+                              SizedBox(
+                                height: 300,
+                                width: 300,
+                                child: PieChart(
+                                    dataMap: dataMap,
+                                    chartType: ChartType.disc),
+                              ),
+                              Container(
+                                  margin: EdgeInsets.only(bottom: 16),
+                                  child: Text("Butuh : " +
+                                      butuhCount.round().toString())),
+                              Text("Ingin : " + inginCount.round().toString()),
+                            ],
+                          ),
                         );
                       }
                     },
@@ -71,8 +84,8 @@ class _PengeluaranState extends State<Pengeluaran> {
                     "Total Pengeluaran : ",
                     style: TextStyle(fontWeight: FontWeight.w800, fontSize: 30),
                   ),
-                  FutureBuilder(
-                    future: database.getAllExpenses(),
+                  StreamBuilder(
+                    stream: database.watchAllExpenses(),
                     builder: (BuildContext context,
                         AsyncSnapshot<dynamic> snapshot) {
                       final allExpense = snapshot.data;
@@ -92,15 +105,59 @@ class _PengeluaranState extends State<Pengeluaran> {
                         Map<String, double> dataMap = new Map();
                         dataMap.putIfAbsent("Butuh", () => butuhCount);
                         dataMap.putIfAbsent("Ingin", () => inginCount);
-                        return SizedBox(
-                          height: 300,
-                          width: 300,
-                          child: PieChart(
-                              dataMap: dataMap, chartType: ChartType.disc),
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 32),
+                          child: Column(
+                            children: <Widget>[
+                              SizedBox(
+                                height: 300,
+                                width: 300,
+                                child: PieChart(
+                                    dataMap: dataMap,
+                                    chartType: ChartType.disc),
+                              ),
+                              Container(
+                                  margin: EdgeInsets.only(bottom: 16),
+                                  child: Text("Total Butuh : " +
+                                      butuhCount.round().toString())),
+                              Text("Total Ingin : " +
+                                  inginCount.round().toString()),
+                            ],
+                          ),
                         );
                       }
                     },
                   ),
+                  StreamBuilder(
+                    stream: database.watchAllExpenses(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<dynamic> snapshot1) {
+                      final allExpense = snapshot1.data;
+                      int total = 0;
+                      for (int i = 0; i < allExpense.length; i++) {
+                        total += allExpense[i].amount;
+                      }
+                      return FutureBuilder(
+                        future: database.getLimit(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<dynamic> snapshot2) {
+                          final limits = snapshot2.data;
+                          if (limits.length == 0) {
+                            return Text("No Limit is Set");
+                          } else {
+                            final limit = limits[0].limit;
+                            return Text(
+                              "Total : " + total.toString(),
+                              style: TextStyle(
+                                  color: total > limit
+                                      ? Colors.red
+                                      : Colors.green),
+                            );
+                          }
+                        },
+                      );
+                    },
+                  )
 
                   //DISINI BUAT YANG PIE CHART
                 ],
@@ -108,8 +165,8 @@ class _PengeluaranState extends State<Pengeluaran> {
             )),
             Card(
                 //FOR FUNCTION, ITERATE ON THIS getThisMonthExpenses
-                child: FutureBuilder(
-              future: database.getAllExpenses(),
+                child: StreamBuilder(
+              stream: database.watchAllExpenses(),
               builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
                 final allExpense = snapshot.data;
                 if (allExpense.length == 0) {
@@ -129,17 +186,29 @@ class _PengeluaranState extends State<Pengeluaran> {
                                 ? FontWeight.bold
                                 : FontWeight.normal),
                       )),
+                      DataCell(RaisedButton(
+                        onPressed: () async {
+                          await database.deleteExpense(expense);
+                        },
+                        child:Text("Delete")
+                      ),
+                      
+                      )
                     ]);
                     dataRowList.add(dataRow);
                   }
-                  return DataTable(
-                    columns: <DataColumn>[
-                      DataColumn(label: Text("Title")),
-                      DataColumn(label: Text("Amount")),
-                      DataColumn(label: Text("Date")),
-                      DataColumn(label: Text("Jenis")),
-                    ],
-                    rows: dataRowList,
+                  return SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(
+                      columns: <DataColumn>[
+                        DataColumn(label: Text("Title")),
+                        DataColumn(label: Text("Amount")),
+                        DataColumn(label: Text("Date")),
+                        DataColumn(label: Text("Jenis")),
+                        DataColumn(label: Text("Delete")),
+                      ],
+                      rows: dataRowList,
+                    ),
                   );
                 }
               },
